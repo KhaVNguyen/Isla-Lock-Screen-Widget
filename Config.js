fe75f07b3953fa23a32ef20bd458d06c3193438d
Kha Nguyen <kha.vng@gmail.com>
2017-02-14 01:51:59 -0800

    Edited some themes

---
+++ Config.js
@@ -0,0 +1,8 @@
+/*
+Configuration here !
+Code by Dacal -- Modified by Schnedi  // D0 NOT REMOVE THIS LINE //
+*/
+
+var Clock = "12h";	       // choose between "12h" or "24h".
+var lang = "en";		   // choose between "ca", "en", "de" or "fr".
+var theme ="dark";
