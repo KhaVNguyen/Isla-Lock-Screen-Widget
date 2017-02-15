adbbcb2f43a28cbd9bf2a225972f5f12794a346b
Kha Nguyen <kha.vng@gmail.com>
2017-02-15 00:03:06 -0800

    Added theme options that are selectable in the settings

--- Config.js
+++
@@ -1,8 +0,0 @@
-/*
-Configuration here !
-Code by Dacal -- Modified by Schnedi  // D0 NOT REMOVE THIS LINE //
-*/
-
-var Clock = "12h";	       // choose between "12h" or "24h".
-var lang = "en";		   // choose between "ca", "en", "de" or "fr".
-var theme ="dark";
